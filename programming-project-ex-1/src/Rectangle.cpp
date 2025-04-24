81b31b3c3e0a625aed39f4d0d329136875f6ca33
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:47:36 -0700

    Create Rectangle.cpp

---
+++ programming-project-ex-1/src/Rectangle.cpp
@@ -0,0 +1,45 @@
+#include "Rectangle.h"
+#include <GL/freeglut.h>
+
+Rectangle::Rectangle() : x(0.0), y(0.0), width(0.4), height(0.4), r(0.0), g(0.0), b(0.0) {
+    //
+}
+
+Rectangle::Rectangle(float xx, float yy, float rr, float gg, float bb) : x(xx), y(yy), width(0.4), height(0.4), r(rr), g(gg), b(bb) {
+    //
+}
+
+void Rectangle::draw() {
+    glColor3f(r, g, b);
+    glBegin(GL_POLYGON);
+        glVertex2f(x - width/2, y + height/2);
+        glVertex2f(x + width/2, y + height/2);
+        glVertex2f(x + width/2, y - height/2);
+        glVertex2f(x - width/2, y - height/2);
+    glEnd();
+}
+
+bool Rectangle::contains(float mx, float my) {
+    if (mx >= x - width/2 && mx <= x + width/2 && my >= y - height/2 && my <= y + height/2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void Rectangle::moveBy(float dx, float dy) {
+    x = x + dx;
+    y = y + dy;
+}
+
+void Rectangle::resize(float factor) {
+    width  = width  * factor;
+    height = height * factor;
+}
+
+void Rectangle::setColor(float nr, float ng, float nb) {
+    r = nr;
+    g = ng;
+    b = nb;
+}
