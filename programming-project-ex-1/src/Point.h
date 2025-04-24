c813c8ae7ba118c5748e11eef3765bb62842417a
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:46:42 -0700

    Create Point.h

---
+++ programming-project-ex-1/src/Point.h
@@ -0,0 +1,26 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include "Shape.h"
+
+class Point : public Shape {
+    float x;
+    float y;
+    float r;
+    float g;
+    float b;
+    int size;
+    
+public:
+    Point();
+    Point(float x, float y, float r, float g, float b, int size);
+
+    void draw() override;
+
+    bool contains(float mx, float my) override;
+    void moveBy(float dx, float dy) override;
+    void resize(float factor) override;
+    void setColor(float nr, float ng, float nb) override;
+};
+
+#endif
