bad6c20a7df2e1e48334286265f63811f1b8926f
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:47:16 -0700

    Create Polygon.h

---
+++ programming-project-ex-1/src/Polygon.h
@@ -0,0 +1,27 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include "Shape.h"
+
+class Polygon : public Shape {
+    float x;
+    float y;
+    int sides;
+    float length;
+    float r;
+    float g;
+    float b;
+
+public:
+    Polygon();
+    Polygon(float x, float y, float r, float g, float b);
+
+    void draw() override;
+
+    bool contains(float mx, float my) override;
+    void moveBy(float dx, float dy) override;
+    void resize(float factor) override;
+    void setColor(float nr, float ng, float nb) override;
+};
+
+#endif
