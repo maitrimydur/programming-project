1993db81c4a6ced7f0dec9aa8ef577c1a0e2a545
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:35:37 -0700

    Create Canvas.h

---
+++ programming-project-ex-1/src/Canvas.h
@@ -0,0 +1,44 @@
+#ifndef CANVAS_H
+#define CANVAS_H
+
+#include <bobcat_ui/all.h>
+#include <vector>
+#include "Shape.h"
+#include "Point.h"
+#include "Rectangle.h"
+#include "Circle.h"
+#include "Triangle.h"
+#include "Polygon.h"
+#include "Scribble.h"
+
+class Canvas : public bobcat::Canvas_ {
+    std::vector<Shape*> shapes;
+    Scribble* currentScribble;
+    Shape* selected;
+
+public:
+    Canvas(int x, int y, int w, int h);
+    ~Canvas();
+
+    void startScribble(float x, float y, float r, float g, float b, int s);
+    void addToScribble(float x, float y, float r, float g, float b, int s);
+    void finishScribble();
+
+    void addRectangle(float x, float y, float r, float g, float b);
+    void addCircle(float x, float y, float r, float g, float b);
+    void addTriangle(float x, float y, float r, float g, float b);
+    void addPolygon(float x, float y, float r, float g, float b);
+
+    void selectAt(float x, float y);
+    void moveSelected(float dx, float dy);
+    void resizeSelected(float factor);
+    void recolorSelected(float r, float g, float b);
+    void bringToFront();
+    void sendToBack();
+
+    void clear();
+    void undo();
+    void render() override;
+};
+
+#endif
