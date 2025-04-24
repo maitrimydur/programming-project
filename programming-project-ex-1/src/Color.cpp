4abdd43caaaa705454305ed9a9c00a73604dd2d3
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:44:44 -0700

    Create Color.cpp

---
+++ programming-project-ex-1/src/Color.cpp
@@ -0,0 +1,19 @@
+#include "Color.h"
+
+Color::Color() {
+    //
+}
+
+Color::Color(float r, float g, float b) {
+    //
+}
+
+float Color::getR() const {
+     return r; 
+}
+float Color::getG() const { 
+    return g; 
+}
+float Color::getB() const { 
+    return b; 
+}
