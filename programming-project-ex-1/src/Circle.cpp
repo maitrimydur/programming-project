668c3cb39cff6a515a5da3d158e809798a45608d
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:36:12 -0700

    Create Circle.cpp

---
+++ programming-project-ex-1/src/Circle.cpp
@@ -0,0 +1,50 @@
+#include "Circle.h"
+#include <GL/freeglut.h>
+#include <cmath>
+
+Circle::Circle() : x(0.0), y(0.0), radius(0.2), r(0.0), g(0.0), b(0.0) {
+  //
+}
+
+Circle::Circle(float xx, float yy, float rr, float gg, float bb) : x(xx), y(yy), radius(0.2), r(rr), g(gg), b(bb) {
+  //
+}
+
+void Circle::draw() {
+    glColor3f(r, g, b);
+    glBegin(GL_POLYGON);
+        float inc = 2 * M_PI / 60;
+        float theta = 0.0;
+        while (theta <= 2 * M_PI) {
+            glVertex2f(x + radius * cos(theta),
+            y + radius * sin(theta));
+            theta = theta + inc;
+        }
+    glEnd();
+}
+
+bool Circle::contains(float mx, float my) {
+    float dx = mx - x;
+    float dy = my - y;
+    if (dx * dx + dy * dy <= radius * radius) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void Circle::moveBy(float dx, float dy) {
+    x = x + dx;
+    y = y + dy;
+}
+
+void Circle::resize(float factor) {
+    radius = radius * factor;
+}
+
+void Circle::setColor(float nr, float ng, float nb) {
+    r = nr;
+    g = ng;
+    b = nb;
+}
