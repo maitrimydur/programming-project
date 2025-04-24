e61d48a576b8fd9f18e654c47758d2bd64ed5b75
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:50:00 -0700

    Create Triangle.h

---
+++ programming-project-ex-1/src/Triangle.h
@@ -0,0 +1,27 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include "Shape.h"
+
+class Triangle : public Shape {
+    float x;
+    float y;
+    float base;
+    float height;
+    float r;
+    float g;
+    float b;
+
+public:
+    Triangle();
+    Triangle(float x, float y, float r, float g, float b);
+
+    void draw() override;
+
+    bool contains(float mx, float my) override;
+    void moveBy(float dx, float dy) override;
+    void resize(float factor) override;
+    void setColor(float nr, float ng, float nb) override;
+};
+
+#endif
