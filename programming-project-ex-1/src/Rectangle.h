49eac2ea1b4e489f072db5e5f4ba9315da3c9515
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:47:49 -0700

    Create Rectangle.h

---
+++ programming-project-ex-1/src/Rectangle.h
@@ -0,0 +1,28 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "Shape.h"
+
+class Rectangle : public Shape {
+    float x;
+    float y;
+    float width;
+    float height;
+    float r;
+    float g;
+    float b;
+
+public:
+    Rectangle();
+    Rectangle(float x, float y, float r, float g, float b);
+
+    void draw() override;
+
+    bool contains(float mx, float my) override;
+    void moveBy(float dx, float dy) override;
+    void resize(float factor) override;
+    void setColor(float nr, float ng, float nb) override;
+
+};
+
+#endif
