0000f01f11ee57ad6f5dd8f02155c9b8f49c66c5
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-24 11:46:19 -0700

    Create Point.cpp

---
+++ programming-project-ex-1/src/Point.cpp
@@ -0,0 +1,43 @@
+#include "Point.h"
+#include <GL/freeglut.h>
+#include <cmath>
+
+Point::Point() : x(0.0), y(0.0), r(0.0), g(0.0), b(0.0), size(7) {
+
+}
+
+Point::Point(float x, float y, float rr, float gg, float bb, int s) : x(x), y(y), r(rr), g(gg), b(bb), size(s) {
+
+}
+
+void Point::draw() {
+    glColor3f(r, g, b);
+    glPointSize(size);
+    glBegin(GL_POINTS);
+        glVertex2f(x, y);
+    glEnd();
+}
+
+bool Point::contains(float mx, float my) {
+    float dx = mx - x;
+    float dy = my - y;
+    float distSq = dx * dx + dy * dy;
+    int half = size / 2;
+    if (distSq <= half * half) return true;
+    else return false;
+}
+
+void Point::moveBy(float dx, float dy) {
+    x = x + dx;
+    y = y + dy;
+}
+
+void Point::resize(float factor) {
+    size = int(size * factor);
+}
+
+void Point::setColor(float nr, float ng, float nb) {
+    r = nr;
+    g = ng;
+    b = nb;
+}
