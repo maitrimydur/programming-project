fd16f4baa700170eccd6b972ab42d4550170c424
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-04-26 13:33:10 -0700

    Update Color.cpp

--- programming-project-ex-1/src/Color.cpp
+++ programming-project-ex-1/src/Color.cpp
@@ -1,19 +1,23 @@
 #include "Color.h"
 
 Color::Color() {
-    //
+    r = 0.0;
+    g = 0.0;
+    b = 0.0;
 }
 
-Color::Color(float r, float g, float b) {
-    //
+Color::Color(float red, float green, float blue) {
+    r = red;
+    g = green;
+    b = blue;
 }
 
-float Color::getR() const {
-     return r; 
+float Color::getR() const { 
+    return r; 
 }
 float Color::getG() const { 
     return g; 
 }
-float Color::getB() const { 
+float Color::getB() const {
     return b; 
 }
