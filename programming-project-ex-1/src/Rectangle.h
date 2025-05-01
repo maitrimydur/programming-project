d3b5646ae9cad95c460a354f9ad88f9ed57ed9d1
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-01 13:39:32 -0700

    Update Rectangle.h

--- programming-project-ex-1/src/Rectangle.h
+++ programming-project-ex-1/src/Rectangle.h
@@ -1,27 +1,27 @@
-#ifndef POLYGON_H
-#define POLYGON_H
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
 
 #include "Shape.h"
 
-class Polygon : public Shape {
+class Rectangle : public Shape {
     float x;
     float y;
-    int sides;
-    float length;
+    float width;
+    float height;
     float r;
     float g;
     float b;
 
 public:
-    Polygon();
-    Polygon(float x, float y, float r, float g, float b);
+    Rectangle();
+    Rectangle(float x, float y, float r, float g, float b);
 
     void draw() override;
-
     bool contains(float mx, float my) override;
     void moveBy(float dx, float dy) override;
     void resize(float factor) override;
     void setColor(float nr, float ng, float nb) override;
+
 };
 
 #endif
