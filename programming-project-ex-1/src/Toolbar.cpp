4edb0ccb6c513842c300a4d48e0cde886b1d088b
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-07 19:41:39 -0700

    Update Toolbar.cpp

--- programming-project-ex-1/src/Toolbar.cpp
+++ programming-project-ex-1/src/Toolbar.cpp
@@ -23,7 +23,6 @@ void Toolbar::clearAction() {
     action = NONE;
 }
 
-
 void Toolbar::visualizeSelectedTool() {
     deselectAllTools();
 
@@ -68,7 +67,6 @@ void Toolbar::visualizeSelectedTool() {
             selectButton -> color(FL_WHITE); 
         }
     }
-
 }
 
 void Toolbar::onClick(bobcat::Widget* sender) {
@@ -131,20 +129,27 @@ ACTION Toolbar::getAction() const {
     return action; 
 }
 
-Toolbar::Toolbar(int x, int y, int w, int h) : Group(x,y,w,h), tool(PENCIL), action(NONE) {
-    pencilButton = new Image(x, y, 50, 50, "./assets/pencil.png");
-    eraserButton = new Image(x, y + 50, 50, 50, "./assets/eraser.png");
-    circleButton = new Image(x, y + 100, 50, 50,"./assets/circle.png");
-    triangleButton = new Image(x, y + 150, 50,50,"./assets/triangle.png");
-    rectangleButton = new Image(x, y + 200, 50,50,"./assets/rectangle.png");
-    polygonButton = new Image(x, y + 250, 50, 50,"./assets/polygon.png");
-    selectButton = new Image(x, y + 300, 50, 50,"./assets/mouse.png");
-    undoButton = new Image(x, y + 350, 50, 50,"./assets/undo.png");
-    clearButton = new Image(x, y + 400, 50, 50,"./assets/clear.png");
-    bringFrontButton = new Image(x, y + 450, 50, 50,"./assets/bring-to-front.png");
-    sendBackButton = new Image(x, y + 500, 50, 50,"./assets/send-to-back.png");
-    plusButton = new Image(x, y + 550, 50, 50, "./assets/plus.png");
-    minusButton = new Image(x, y + 600, 50, 50, "./assets/minus.png");
+Toolbar::Toolbar(int x, int y, int w, int h) : Group(x, y, w, h), tool(PENCIL), action(NONE) {
+    pencilButton = new Image(x + 10, y + 50, 50, 50, "./assets/pencil.png");
+    eraserButton = new Image(x + 70, y + 50, 50, 50, "./assets/eraser.png");
+    selectButton = new Image(x + 130, y + 50, 50, 50,"./assets/mouse.png");
+    undoButton = new Image(x + 10, y + 110, 50, 50,"./assets/undo.png");
+    clearButton = new Image(x + 70, y + 110, 50, 50,"./assets/clear.png");
+    bringFrontButton = new Image(x + 130, y + 110, 50, 50,"./assets/bring-to-front.png");
+    sendBackButton = new Image(x + 10, y + 170, 50, 50,"./assets/send-to-back.png");
+
+
+    circleButton = new Image(x + 10, y + 290, 50, 50,"./assets/circle.png");
+    triangleButton = new Image(x + 70, y + 290, 50,50,"./assets/triangle.png");
+    rectangleButton = new Image(x + 10, y + 350, 50,50,"./assets/rectangle.png");
+    polygonButton = new Image(x + 70, y + 350, 50, 50,"./assets/polygon.png");
+
+    plusButton = new Image(x + 170, y + 290, 50, 50, "./assets/plus.png");
+    minusButton = new Image(x + 170, y + 350, 50, 50, "./assets/minus.png");
+
+
+    
+    
 
     tool = PENCIL;
     action = NONE;
