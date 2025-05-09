c7bd09e817848ea2b6391032827e28eb87151b92
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 01:02:51 -0700

    Update Canvas.h

--- programming-project-ex-1/src/Canvas.h
+++ programming-project-ex-1/src/Canvas.h
@@ -1,47 +1,66 @@
-#ifndef CANVAS_H
-#define CANVAS_H
-
-#include <bobcat_ui/all.h>
-#include <vector>
-#include "Shape.h"
-#include "Point.h"
-#include "Rectangle.h"
 #include "Circle.h"
-#include "Triangle.h"
-#include "Polygon.h"
-#include "Scribble.h"
+#include <GL/freeglut.h>
+#include <cmath>
 
-class Canvas : public bobcat::Canvas_ {
-    std::vector<Shape*> shapes;
-    Scribble* currentScribble;
-    Shape* selected;
+Circle::Circle() {
+    x = 0.0;
+    y = 0.0;
+    radius = 0.1;
+    r = 0.0;
+    g = 0.0;
+    b = 0.0;
+}
+
+Circle::Circle(float mx, float my, float red, float green, float blue) : Circle() {
+    x = mx;
+    y = my;
+    radius = 0.1;
+    r = red;
+    g = green;
+    b = blue;
+}
+
+void Circle::draw() {
+    glColor3f(r, g, b);
+    glBegin(GL_POLYGON);
+        float inc = 2 * M_PI / 60;
+        float theta = 0.0;
+
+        while (theta <= 2 * M_PI) {
+            glVertex2f(x + radius * cos(theta), y + radius * sin(theta));
+            theta = theta + inc;
+        }
+
+    glEnd();
+}
+
+bool Circle::contains(float mx, float my) {
+    float dx = mx - x;
+    float dy = my - y;
     
-public:
-    Canvas(int x, int y, int w, int h);
-    ~Canvas();
+    if (dx * dx + dy * dy <= radius * radius) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
-    void startScribble(float x, float y, float r, float g, float b, int size);
-    void addToScribble(float x, float y, float r, float g, float b, int size);
-    void finishScribble();
+void Circle::moveBy(float dx, float dy) {
+    x = x + dx;
+    y = y + dy;
+}
 
-    void addRectangle(float x, float y, float r, float g, float b);
-    void addCircle(float x, float y, float r, float g, float b);
-    void addTriangle(float x, float y, float r, float g, float b);
-    void addPolygon(float x, float y, float r, float g, float b);
+void Circle::resize(float factor) {
+    radius = radius * factor;
 
-    void selectAt(float x, float y);
-    void moveSelected(float dx, float dy);
-    void resizeSelected(float factor);
-    void recolorSelected(float r, float g, float b);
-    void bringToFront();
-    void sendToBack();
-    void resizeSelectedUp();
-    void resizeSelectedDown();
+    if (radius < 0.05) {
+        radius = 0.05;
+    }
+}
 
-    void clear();
-    void undo();
-    void render() override;
-    void eraseAt(float x, float y);
-};
-
-#endif
+void Circle::setColor(float nr, float ng, float nb) {
+    r = nr;
+    g = ng;
+    b = nb;
+}
