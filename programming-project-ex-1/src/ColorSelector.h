a80f90cba5b2b0c8051fff391eabc21678bc4e83
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 01:04:17 -0700

    Update ColorSelector.h

--- programming-project-ex-1/src/ColorSelector.h
+++ programming-project-ex-1/src/ColorSelector.h
@@ -2,6 +2,7 @@
 #define COLOR_SELECTOR_H
 
 #include <bobcat_ui/all.h>
+#include <bobcat_ui/textbox.h>
 #include "Color.h"
 #include "Enums.h"
 
@@ -13,16 +14,46 @@ class ColorSelector : public bobcat::Group {
     bobcat::Button* blueButton;
     bobcat::Button* indigoButton;
     bobcat::Button* violetButton;
+    bobcat::Button* pinkButton;
+    bobcat::Button* whiteButton;
+    bobcat::Button* lightGreyButton;
+    bobcat::Button* darkGreyButton;
+    bobcat::Button* blackButton;
+
+    bobcat::Button* increaseRedButton;
+    bobcat::Button* decreaseRedButton;
+    bobcat::Button* increaseGreenButton;
+    bobcat::Button* decreaseGreenButton;
+    bobcat::Button* increaseBlueButton;
+    bobcat::Button* decreaseBlueButton;
+    bobcat::Button* clearButton;
+    bobcat::Button* confirmButton;
+
+    bobcat::TextBox* colorsText;
+    bobcat::TextBox* redColorText;
+    bobcat::IntInput* redColorValue;
+    bobcat::TextBox* greenColorText;
+    bobcat::IntInput* greenColorValue;
+    bobcat::TextBox* blueColorText;
+    bobcat::IntInput* blueColorValue;
 
     COLOR color;
-    
+
+    bool usingCustomColor;
+    int lastConfirmedR;
+    int lastConfirmedG;
+    int lastConfirmedB;
+
     void deselectAllColors();
     void visualizeSelectedColor();
     void onClick(bobcat::Widget* sender);
-
+    void onAdjustColor(bobcat::Widget* sender);
+    void onClearInputs(bobcat::Widget* sender);
+    void onConfirmColor(bobcat::Widget* sender);
+    
 public:
+    void updateRGBInputs();
     ColorSelector(int x, int y, int w, int h);
-
     Color getColor() const;
 };
 
