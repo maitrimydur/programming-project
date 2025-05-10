a8508162fed7d0429672494f4f1581b4cd7f4d68
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:51:21 -0700

    Update Shape.cpp

--- programming-project-ex-1/src/Shape.cpp
+++ programming-project-ex-1/src/Shape.cpp
@@ -1,9 +1,11 @@
 #include "Shape.h"
 
+// Virtual destructor that ensures that the derived destructors are called correctly
 Shape::~Shape() {
     //
 }
 
+// Makes sure that only Scribble returns true
 bool Shape::isScribble() const {
     return false;
 }
