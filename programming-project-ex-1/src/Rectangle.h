83c27faac59abb613a3f7d66bd2737a4045b0161
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:50:26 -0700

    Update Rectangle.h

--- programming-project-ex-1/src/Rectangle.h
+++ programming-project-ex-1/src/Rectangle.h
@@ -3,6 +3,7 @@
 
 #include "Shape.h"
 
+// Rectangle that is a centered shape with configurable size and color
 class Rectangle : public Shape {
     float x;
     float y;
@@ -13,11 +14,15 @@ class Rectangle : public Shape {
     float b;
 
 public:
+// Constructors that are default or positioned with a custom color
     Rectangle();
     Rectangle(float x, float y, float r, float g, float b);
 
+    // Rendering and hit-testing methods
     void draw() override;
     bool contains(float mx, float my) override;
+
+    // Transformation and styling methods
     void moveBy(float dx, float dy) override;
     void resize(float factor) override;
     void setColor(float nr, float ng, float nb) override;
