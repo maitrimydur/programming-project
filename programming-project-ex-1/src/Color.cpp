779abb0d6ecbdbe28bfec0119a6f74ac1ff03f8c
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:47:17 -0700

    Update Color.cpp

--- programming-project-ex-1/src/Color.cpp
+++ programming-project-ex-1/src/Color.cpp
@@ -1,17 +1,20 @@
 #include "Color.h"
 
+// Default constructor zeros all Color channel storage
 Color::Color() {
     r = 0.0;
     g = 0.0;
     b = 0.0;
 }
 
+// Parameterized sets the give values
 Color::Color(float red, float green, float blue) : Color() {
     r = red;
     g = green;
     b = blue;
 }
 
+// Accessors that return individual color channel value
 float Color::getR() const { 
     return r; 
 }
