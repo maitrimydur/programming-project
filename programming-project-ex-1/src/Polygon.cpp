3b11021a6366b15690b9ec943f88a595e16c92b4
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:49:16 -0700

    Update Polygon.cpp

--- programming-project-ex-1/src/Polygon.cpp
+++ programming-project-ex-1/src/Polygon.cpp
@@ -2,6 +2,7 @@
 #include <GL/freeglut.h>
 #include <cmath>
 
+// Default constructor which initializes the Polygon's center at (0,0), has 5 sides, creates a default size and is the color black
 Polygon::Polygon() {
     x = 0.0;
     y = 0.0;
@@ -12,6 +13,7 @@ Polygon::Polygon() {
     b = 0.0;
 }
 
+// Parameterized constructor that sets the center, keeps the default shape and the color values and then applies it with the provided color
 Polygon::Polygon(float mx, float my, float red, float green, float blue) : Polygon() {
     x = mx;
     y = my;
@@ -22,6 +24,7 @@ Polygon::Polygon(float mx, float my, float red, float green, float blue) : Polyg
     b = blue;
 }
 
+// Renders the regular polygon with its current color
 void Polygon::draw() {
     glColor3f(r, g, b);
     glBegin(GL_POLYGON);
@@ -35,6 +38,7 @@ void Polygon::draw() {
     glEnd();
 }
 
+// Simple hit-test that is within the polygon's bounding box
 bool Polygon::contains(float mx, float my) {
     float dx = mx - x;
     float dy = my - y;
@@ -47,11 +51,13 @@ bool Polygon::contains(float mx, float my) {
     }
 }
 
+// Translates the polygon center position by (dx, dy)
 void Polygon::moveBy(float dx, float dy) {
     x = x + dx;
     y = y + dy;
 }
 
+// Scales the edge length by the factor, enforcing its minimum length
 void Polygon::resize(float factor) {
     length = length * factor;
     
@@ -60,6 +66,7 @@ void Polygon::resize(float factor) {
     }
 }
 
+// Updates the polygon's RGB values
 void Polygon::setColor(float nr, float ng, float nb) {
     r = nr;
     g = ng;
