88b4f2a7c6937a65784eb2a7a79f17ed771b0ac7
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:47:30 -0700

    Update Color.h

--- programming-project-ex-1/src/Color.h
+++ programming-project-ex-1/src/Color.h
@@ -1,15 +1,18 @@
 #ifndef COLOR_H 
 #define COLOR_H
 
+// Gives a storage for the color channels
 class Color {
     float r;
     float g;
     float b;
 
 public:
+    // Constructors with default color as black and parameterized the channels
     Color();
     Color(float r, float g, float b);
 
+    // Accessor that retrieves individual channel values
     float getR() const;
     float getG() const;
     float getB() const;
