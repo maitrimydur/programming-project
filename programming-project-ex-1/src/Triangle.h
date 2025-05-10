4e49afb30f245b6d0fb5c755f9cff4a4505f0a6b
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:52:36 -0700

    Update Triangle.h

--- programming-project-ex-1/src/Triangle.h
+++ programming-project-ex-1/src/Triangle.h
@@ -3,6 +3,7 @@
 
 #include "Shape.h"
 
+// Triangle is an isosceles triangle shape with the position, the size, and the color
 class Triangle : public Shape {
     float x;
     float y;
@@ -13,11 +14,15 @@ class Triangle : public Shape {
     float b;
 
 public:
+    // Constructors that have default centered as a black triangle or positioned and colored
     Triangle();
     Triangle(float x, float y, float r, float g, float b);
 
+    // Drawing and hit-testing methods
     void draw() override;
     bool contains(float mx, float my) override;
+    
+    // Transformation and styling methods
     void moveBy(float dx, float dy) override;
     void resize(float factor) override;
     void setColor(float nr, float ng, float nb) override;
