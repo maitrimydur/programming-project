07f77f0aa868a864c0cec3e30fbd3a3c84e1de0e
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:48:28 -0700

    Update Enums.h

--- programming-project-ex-1/src/Enums.h
+++ programming-project-ex-1/src/Enums.h
@@ -1,10 +1,13 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
+// Tools that are available to draw and to change modes 
 enum TOOL {PENCIL, ERASER, CIRCLE, TRIANGLE, RECTANGLE, POLYGON, SELECT};
 
+// Colors that are available to select with a custom color maker 
 enum COLOR {RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, PINK, WHITE, LIGHT_GREY, DARK_GREY, BLACK, CUSTOM};
 
+// Actions that canvas commands when triggered by toolbar
 enum ACTION {NONE, UNDO, CLEAR, BRING_TO_FRONT, SEND_TO_BACK, RESIZE_UP, RESIZE_DOWN};
 
 #endif
