f211214bd5f8d32867ca13e1f89cf37b700f7cf2
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:49:29 -0700

    Update Polygon.h

--- programming-project-ex-1/src/Polygon.h
+++ programming-project-ex-1/src/Polygon.h
@@ -3,6 +3,7 @@
 
 #include "Shape.h"
 
+// Polygon that represents a regular n-sided polygon (default 5 sides) with a position, size, and color
 class Polygon : public Shape {
     float x;
     float y;
@@ -13,11 +14,15 @@ class Polygon : public Shape {
     float b;
 
 public:
+    // Constructors that are default polygon or parameterized positioned with custom color
     Polygon();
     Polygon(float x, float y, float r, float g, float b);
 
+    // Draws and hit-tests methods
     void draw() override;
     bool contains(float mx, float my) override;
+
+    // Transformation and styling methods
     void moveBy(float dx, float dy) override;
     void resize(float factor) override;
     void setColor(float nr, float ng, float nb) override;
