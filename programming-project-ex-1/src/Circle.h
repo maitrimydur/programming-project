1766651cc0721569c6de394de5035525011aa626
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:47:01 -0700

    Update Circle.h

--- programming-project-ex-1/src/Circle.h
+++ programming-project-ex-1/src/Circle.h
@@ -3,6 +3,7 @@
 
 #include "Shape.h"
 
+// Circle implements based on the Shape class and stores position, size, and color
 class Circle : public Shape {
     float x;
     float y;
@@ -12,9 +13,11 @@ class Circle : public Shape {
     float b;
     
 public:
+    // Default constructor and parameterized constructor
     Circle();                                              
     Circle(float x, float y, float r, float g, float b);
 
+    // Methods to help draw and interact with circle
     void draw() override;
     bool contains(float mx, float my) override;
     void moveBy(float dx, float dy) override;
