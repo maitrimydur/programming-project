24503ff447b96200f4ac70faee582c21e82bc157
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:51:36 -0700

    Update Shape.h

--- programming-project-ex-1/src/Shape.h
+++ programming-project-ex-1/src/Shape.h
@@ -4,21 +4,19 @@
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 
+// Shape is an abstract base class which defines the interface for all drawable objects
 class Shape {
 
 public: 
+    // Core operations
     virtual void draw() = 0;
-
     virtual bool contains(float mx, float my) = 0;
-
     virtual void moveBy(float dx, float dy) = 0;
-
     virtual void resize(float factor) = 0;
-
     virtual void setColor(float r, float g, float b) = 0;
 
+    // Identification and cleanup
     virtual bool isScribble() const;
-
     virtual ~Shape();
 };
 
