c0e7c50a947dc9597c4d7e91bd50adace39abfda
Maithreyi Mydur <121923992+maitrimydur@users.noreply.github.com>
2025-05-09 18:48:59 -0700

    Update Point.h

--- programming-project-ex-1/src/Point.h
+++ programming-project-ex-1/src/Point.h
@@ -3,6 +3,7 @@
 
 #include "Shape.h"
 
+// Creates point with a drawable dot that is used for a freehand scribble
 class Point : public Shape {
     float x;
     float y;
@@ -12,11 +13,15 @@ class Point : public Shape {
     int size;
     
 public:
+    // Default and parameterized constructors that initialize the points
     Point();
     Point(float x, float y, float r, float g, float b, int size);
 
+    // Draws the point and tests if the coordinate lies within its area
     void draw() override;
     bool contains(float mx, float my) override;
+
+    // Moves, resizes, and recolors the point
     void moveBy(float dx, float dy) override;
     void resize(float factor) override;
     void setColor(float nr, float ng, float nb) override;
